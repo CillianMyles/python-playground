457317c5feedba7345abed468cbb3948678d1928
Cillian Myles <myles.cillian@gmail.com>
2026-08-14 19:56:18 +0100

    feat: generate JWT

---
+++ 22_generate_jwt/main.py
@@ -0,0 +1,29 @@
+import jwt
+import time
+
+
+def main():
+    secret = "extra-super-special-secret-sauce"
+    created_at = int(time.time())
+    expires_at = created_at + 3600
+    payload = {
+        "sub": "user123",
+        "email": "getme@cillianmyles.com",
+        "sub": "user123",
+        "iat": created_at,
+        "exp": expires_at,
+    }
+    token = jwt.encode(
+        payload,
+        secret,
+        algorithm="HS256",
+    )
+    print("")
+    print("Generated JWT...")
+    print("")
+    print(token)
+    print("")
+
+
+if __name__ == "__main__":
+    main()
